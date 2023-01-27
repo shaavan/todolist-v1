3472d2f9d81f68669bab45ca87e9cc0ba36cc1f5
shaavan <shaavan.github@gmail.com>
2023-01-27 21:47:46 +0530

    Added style sheet to the ejs template
    - Moved the css file to the public folder to be able to render on the
      front end side.
    - Add a work list as well on the "/work" path.

--- app.js
+++ app.js
@@ -3,26 +3,52 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 const port = process.env.PORT || 3000;
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res) {
-    var today = new Date();
+    let today = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var day = today.toLocaleDateString("en-us", options);
-    res.render("list", {dayName: day, itemsListName: items});
+    let date = today.toLocaleDateString("en-us", options);
+    res.render("list", {listName: date, itemsListName: items});
 });
 
 app.post("/", function(req, res) {
-    var item = req.body.item;
-    items.push(item);
+    let item = req.body.item;
+    console.log(req.body);
 
-    res.redirect("/");
+    if(req.body.button === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
+
+app.get("/work", function(req, res) {
+    res.render("list", {listName: "Work List", itemsListName: workItems});
+})
+
+// The following post is not used right now.
+// The action on the list.ejs posts to the "/" path and not to the "/work",
+// However, a conditional logic has been added to the app.post("/"), that will check
+// on which route the new item button button was pressed by tapping into the req.body.button
+// property of the req, which tracks the value the submit button has.
+// And the value of submit button has been set to equal to the "listName" variable.
+
+// app.post("/work", function(req, res) {
+//     let item = req.body.item;
+//     workItems.push(item);
+
+//     res.redirect("/work");
+// })
 
 app.listen(port, function() {
     console.log("Listening on port", port);
